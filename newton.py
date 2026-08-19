3d4ab0087775fb8ae1b0cccded0ad0971dbc2dd4
Neha Suresh <neha_suresh@berkeley.edu>
2026-08-19 12:03:43 -0700

    Rearranging function order on newton.py so that the first and second derivative functions come before the optimize function

--- newton.py
+++ newton.py
@@ -1,3 +1,11 @@
+def first_derivative(value, fun, step_size):
+    # implement finite difference approach to estimate derivative 
+    return (fun(value + step_size) - fun(value)) / step_size
+
+def second_derivative(value, fun, step_size):
+    # implement finite difference approach to estimate derivative 
+    return (fun(value + 2 * step_size) - (2 * fun(value + step_size)) + fun(value)) / (step_size**2)
+
 def optimize(start, fun, stop_criteria = 0.1, step_size = 0.1):
 
     # calculate next_value using start 
@@ -12,11 +20,3 @@ def optimize(start, fun, stop_criteria = 0.1, step_size = 0.1):
     # recursive case
     return optimize(next_value, fun, stop_criteria, step_size)
 
-
-def first_derivative(value, fun, step_size):
-    # implement finite difference approach to estimate derivative 
-    return (fun(value + step_size) - fun(value)) / step_size
-
-def second_derivative(value, fun, step_size):
-    # implement finite difference approach to estimate derivative 
-    return (fun(value + 2 * step_size) - (2 * fun(value + step_size)) + fun(value)) / (step_size**2)
